552d42d97f455b4151f3280d646f4a0f17c0c104
Tanya Prado <tanyaprado@gmail.com>
2022-12-21 20:22:44 -0800

    good fail for marsAge calculator

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,9 +2,10 @@ import { Calculator } from './../src/calculator.js';
 
 describe('Calculator', () => {
 
-  test('should create an object with properties that calculate an inputted age into mercury and venus years', () => {
+  test('should create an object with properties that calculate an inputted age into Mercury, Venus and Mars years', () => {
     const spacePerson = new Calculator(75);
     expect(spacePerson.mercuryAge).toEqual(18);
     expect(spacePerson.venusAge).toEqual(46.5);
+    expect(spacePerson.marsAge).toEqual(141);
   });
 });
