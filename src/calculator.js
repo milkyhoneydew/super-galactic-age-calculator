7402dd102c532aeda623aea28a05f78412103f2b
Tanya Prado <tanyaprado@gmail.com>
2022-12-21 20:25:02 -0800

    good fail for marsAge calculator

--- src/calculator.js
+++ src/calculator.js
@@ -2,5 +2,6 @@ export class Calculator {
   constructor(age) {
       this.mercuryAge = parseInt(age) * .24;
       this.venusAge = parseInt(age) * .62;
+      this.marsAge = parseInt(age) * 1.88;
   }
 };
