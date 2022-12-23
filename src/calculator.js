a7676f9372de553ebe3cd83d0b16dd7c140ba2da
Tanya Prado <tanyaprado@gmail.com>
2022-12-23 12:29:26 -0800

    pass earth years test for yearsSinceBirthday prototype method

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,14 @@
 export class Calculator {
   
   constructor(age) {
-      this.mercuryAge = parseInt(age) * .24;
-      this.venusAge = parseInt(age) * .62;
-      this.marsAge = parseInt(age) * 1.88;
-      this.jupiterAge = parseInt(age) * 11.86;
+    this.age = parseInt(age);
+    this.mercuryAge = parseInt(age) * .24;
+    this.venusAge = parseInt(age) * .62;
+    this.marsAge = parseInt(age) * 1.88;
+    this.jupiterAge = parseInt(age) * 11.86;
+  };
+
+  yearsSinceBirthday(birthday) {
+    return (this.age - birthday);
   }
 };
