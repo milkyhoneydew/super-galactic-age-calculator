b67f20abbad94e432afb8bfe144f1135ef20c088
Tanya Prado <tanyaprado@gmail.com>
2023-01-07 14:30:57 -0800

    edit variable names and consolidate variables in push methods

--- src/calculator.js
+++ src/calculator.js
@@ -6,41 +6,35 @@ export class Calculator {
     this.venusAge = parseInt(age) * .62;
     this.marsAge = parseInt(age) * 1.88;
     this.jupiterAge = parseInt(age) * 11.86;
-  };
+  }
 
   yearsSinceBirthday(birthday) {
     this.yearsPassed = [];
     let earthYears = this.age - birthday;
-    this.yearsPassed.push((earthYears).toString());
+    let earthString = earthYears.toString();
     let mercury = ((earthYears) / .24).toString();
-    let mercurySlice = mercury.slice(0, (mercury.indexOf(".")) + 3);
-    this.yearsPassed.push(mercurySlice);
+    let mercuryString = mercury.slice(0, (mercury.indexOf(".")) + 3);
     let venus = ((earthYears) / .62).toString();
-    let venusSlice = venus.slice(0, (venus.indexOf(".")) + 3);
-    this.yearsPassed.push(venusSlice);
+    let venusString = venus.slice(0, (venus.indexOf(".")) + 3);
     let mars = ((earthYears) / 1.88).toString();
-    let marsSlice = mars.slice(0, (mars.indexOf(".")) + 3);
-    this.yearsPassed.push(marsSlice);
+    let marsString = mars.slice(0, (mars.indexOf(".")) + 3);
     let jupiter = ((earthYears) / 11.86).toString();
-    let jupiterSlice = jupiter.slice(0, (jupiter.indexOf(".")) + 3);
-    this.yearsPassed.push(jupiterSlice);
-  };
+    let jupiterString = jupiter.slice(0, (jupiter.indexOf(".")) + 3);
+    this.yearsPassed.push(earthString, mercuryString, venusString, marsString, jupiterString);
+  }
 
   yearsUntilBirthday(birthday) {
     this.yearsUntil = [];
     let earthYears = birthday - this.age;
-    this.yearsUntil.push((earthYears).toString());
+    let earthString = earthYears.toString();
     let mercury = ((earthYears) / .24).toString();
-    let mercurySlice = mercury.slice(0, (mercury.indexOf(".")) + 3);
-    this.yearsUntil.push(mercurySlice);
+    let mercuryString = mercury.slice(0, (mercury.indexOf(".")) + 3);
     let venus = ((earthYears) / .62).toString();
-    let venusSlice = venus.slice(0, (venus.indexOf(".")) + 3);
-    this.yearsUntil.push(venusSlice);
+    let venusString = venus.slice(0, (venus.indexOf(".")) + 3);
     let mars = ((earthYears) / 1.88).toString();
-    let marsSlice = mars.slice(0, (mars.indexOf(".")) + 3);
-    this.yearsUntil.push(marsSlice);
+    let marsString = mars.slice(0, (mars.indexOf(".")) + 3);
     let jupiter = ((earthYears) / 11.86).toString();
-    let jupiterSlice = jupiter.slice(0, (jupiter.indexOf(".")) + 3);
-    this.yearsUntil.push(jupiterSlice);
-  };
+    let jupiterString = jupiter.slice(0, (jupiter.indexOf(".")) + 3);
+    this.yearsUntil.push(earthString, mercuryString, venusString, marsString, jupiterString);
+  }
 }
